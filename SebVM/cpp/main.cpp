d8f9adc44225e76bbcc556d30ea1cccff22c5b38
sebs-rotors <sebisidor@Sebastians-MacBook-Pro.local>
2026-03-07 17:18:20 -0800

    Successfully integrated Qt for GUI

--- SebVM/cpp/main.cpp
+++ SebVM/cpp/main.cpp
@@ -5,3 +5,20 @@
 //  Created by Sebastian Sidor on 3/7/26.
 //
 
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QWidget>
+#include <QtWidgets/QLabel>
+
+int main(int argc, char* argv[]) {
+    QApplication app(argc, argv);
+    
+    QWidget window;
+    window.setWindowTitle("SebVM");
+    window.resize(400, 300);
+    
+    QLabel label("Qt is working!", &window);
+    label.move(150, 150);
+    
+    window.show();
+    return app.exec();
+}
